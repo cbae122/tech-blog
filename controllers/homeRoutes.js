a018a11556c9e429ad032cdb788c0f074b7872c5
cbae <xchris.bae@gmail.com>
2023-06-03 00:31:59 -0700

    updating homeroutes and adding single post handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const sequelize = require('../config/connection');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
+// all posts on homepage
 router.get('/', async (req, res) => {
   try {
     const allPostData = await Post.findAll({
@@ -13,13 +14,33 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // try {
-  //   res.render('homepage');
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
 });
 
+// one post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const onePostData = await Post.findOne({
+      where: { id: req.params.id },
+      include: [
+        User, 
+        { 
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (onePostData) {
+      const post = onePostData.get({ plain: true });
+      console.log(post);
+      res.render()
+    }
+    const posts = allPostData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { posts, logged_in: req.session.logged_in});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // login
 router.get('/login', (req, res) => {
