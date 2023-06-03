9c0c265eacc9131c7410dd9f11a77953cd959e87
cbae <xchris.bae@gmail.com>
2023-06-02 22:34:02 -0700

    dashboard working

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postDB = await Post.findAll({
+            where: {'user_id': req.session.user_id},
+            include: [User]
+        });
+        const posts = postDB.map((post) => post.get({ plain: true }));
+        console.log(posts);
+
+        res.render('dashboard', { posts, logged_in: true });
+    } catch (err) {
+        res.redirect('/login')
+    }
+});
 
 module.exports = router;
