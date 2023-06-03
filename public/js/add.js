1aae4028c4e0e5bab8affe9d74148f2b968c7fcf
cbae <xchris.bae@gmail.com>
2023-06-03 13:17:21 -0700

    added addjs for new posts

---
+++ public/js/add.js
@@ -0,0 +1,28 @@
+const newFormHandler = async function(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+
+    console.log(title);
+    console.log(content);
+
+    const response = await fetch (`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
