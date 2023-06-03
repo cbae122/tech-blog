ff6587bd3acfb73843c7d4f81d7c0a3fe1bed0b1
cbae <xchris.bae@gmail.com>
2023-06-02 20:12:31 -0700

    added index models

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const sequelize = require('../config/connection');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage');
+    const allPostData = await Post.findAll({
+      include: [User],
+    });
+    const posts = allPostData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { posts, logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
+  // try {
+  //   res.render('homepage');
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
+
+// login
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
@@ -17,6 +30,7 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+// signup
 
 router.get('/signup', (req, res) => {
   res.render('signup');
